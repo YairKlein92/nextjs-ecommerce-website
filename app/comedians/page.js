1326ee0d74afea10dc5d128ee019ebb142979ed8
Csaba Klein <sympla@gmail.com>
2023-02-03 09:59:26 +0100

    Fix kinda comedian site
    Need further clearing, it is not perfect...

--- app/comedians/page.js
+++ app/comedians/page.js
@@ -11,70 +11,68 @@ export default function ComediansPage() {
       <main>
         {comedians.map((comedian) => {
           return (
-            <div
-              className={styles.comedianDiv}
-              key={comedian.id}
-              display="flex"
-            >
-              <Link
-                href={`/comedians/${comedian.lastName.toLocaleLowerCase()}`}
-              >
-                <Image
-                  src={`/${comedian.firstName}.webp`}
-                  alt={comedian.lastName}
-                  width="112"
-                  height="80"
-                />
-              </Link>
-              <div className={styles.intro}>
+            <div className={styles.mainDiv} key={comedian.id}>
+              <div className={styles.comedianDiv} display="flex">
                 <Link
                   href={`/comedians/${comedian.lastName.toLocaleLowerCase()}`}
                 >
-                  <h2 key={comedian.id}>
-                    {comedian.firstName} {comedian.lastName}
-                  </h2>
+                  <Image
+                    src={`/${comedian.firstName}.webp`}
+                    alt={comedian.lastName}
+                    width="112"
+                    height="80"
+                  />
                 </Link>
-                <p>"{comedian.lastSpecial}" world tour</p>
-                <div className={styles.span}>09.01.2023 - 04.04.2023</div>
-                <div className={styles.span}>
-                  {' '}
-                  {comedian.eventNumber} events in Vienna
+                <div className={styles.intro}>
+                  <Link
+                    href={`/comedians/${comedian.lastName.toLocaleLowerCase()}`}
+                  >
+                    <h2 key={comedian.id}>
+                      {comedian.firstName} {comedian.lastName}
+                    </h2>
+                  </Link>
+                  <p>"{comedian.lastSpecial}" world tour</p>
+                  <div className={styles.span}>09.01.2023 - 04.04.2023</div>
+                  <div className={styles.span}>
+                    {' '}
+                    {comedian.eventNumber} events in Vienna
+                  </div>
                 </div>
-              </div>
-              <div className={styles.icons}>
-                <a href="/">
-                  <Image
-                    src="/save.png"
-                    alt="Save to your computer"
-                    title="Save to your computer"
-                    width="25"
-                    height="25"
-                  />
-                </a>
-                <a href="/">
-                  <Image
-                    src="/phone.png"
-                    alt="Download to your mobile device"
-                    title="Download to your mobile device"
-                    width="25"
-                    height="25"
-                  />
-                </a>
-                <a href="/">
-                  <Image
-                    src="/print.png"
-                    alt="Print it out"
-                    title="Print it out"
-                    width="25"
-                    height="25"
-                  />
-                </a>
-              </div>
-              <div className={styles.purchaseDiv}>
-                <span>Tickets from {comedian.ticketPriceMin}</span>
-                <a href="/">
-                  <span className={styles.buyOneNow}>Buy one now!</span>
-                </a>
+                <div className={styles.icons}>
+                  <a href="/">
+                    <Image
+                      src="/save.png"
+                      alt="Save to your computer"
+                      title="Save to your computer"
+                      width="25"
+                      height="25"
+                    />
+                  </a>
+                  <a href="/">
+                    <Image
+                      src="/phone.png"
+                      alt="Download to your mobile device"
+                      title="Download to your mobile device"
+                      width="25"
+                      height="25"
+                    />
+                  </a>
+                  <a href="/">
+                    <Image
+                      src="/print.png"
+                      alt="Print it out"
+                      title="Print it out"
+                      width="25"
+                      height="25"
+                    />
+                  </a>
+                </div>
+                <div className={styles.purchaseDiv}>
+                  <span>Tickets from {comedian.ticketPriceMin}</span>
+                  <a href="/">
+                    <span className={styles.buyOneNow}>Buy one now!</span>
+                  </a>
+                </div>
               </div>
             </div>
           );
