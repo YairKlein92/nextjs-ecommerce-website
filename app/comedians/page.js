def728417a6afd2325d7fc0124aec68637ce983d
Csaba Klein <sympla@gmail.com>
2023-02-10 20:49:42 +0100

    Create Flipping Card

--- app/comedians/page.js
+++ app/comedians/page.js
@@ -37,64 +37,74 @@ export default async function ComediansPage() {
         {ticketsInCart.map((comedian) => {
           return (
             <div className={styles.mainDiv} key={comedian.id}>
-              <div className={styles.comedianDiv}>
-                {/* <Link href={`/comedians/${comedian.id}`}> */}
-                <h3 key={comedian.id}>
-                  {comedian.firstName} {comedian.lastName}
-                </h3>
-                <Image
-                  src={`/${comedian.firstName}.webp`}
-                  alt={comedian.lastName}
-                  width="112"
-                  height="80"
-                />
-                {/* </Link> */}
+              <div className={styles.comedianDivCard}>
+                <div
+                  className={`${styles.comedianDiv} ${styles.comedianDivFront}`}
+                >
+                  {/* <Link href={`/comedians/${comedian.id}`}> */}
+                  <h3 key={comedian.id}>
+                    {comedian.firstName} {comedian.lastName}
+                  </h3>
+                  <Image
+                    src={`/${comedian.firstName}.webp`}
+                    alt={comedian.lastName}
+                    width="112"
+                    height="80"
+                  />
+                  {/* </Link> */}
 
-                <div className={styles.intro}>
-                  <p>"{comedian.lastSpecial}" world tour</p>
-                  <div className={styles.span}>09.01.2023 - 04.04.2023</div>
-                  <div className={styles.span}>
-                    {' '}
-                    {comedian.eventNumber} events in Vienna
+                  <div className={styles.intro}>
+                    <p>"{comedian.lastSpecial}" world tour</p>
+                    <div className={styles.span}>09.01.2023 - 04.04.2023</div>
+                    <div className={styles.span}>
+                      {' '}
+                      {comedian.eventNumber} events in Vienna
+                    </div>
+                    <div className={styles.span}>
+                      Tickets:{comedian.ticketAmount}
+                    </div>
                   </div>
-                  <div className={styles.span}>
-                    Tickets:{comedian.ticketAmount}
+                  <div className={styles.icons}>
+                    <a href="/cart">
+                      <Image
+                        src="/save.png"
+                        alt="Save to your computer"
+                        title="Save to your computer"
+                        width="25"
+                        height="25"
+                      />
+                    </a>
+                    <a href="/cart">
+                      <Image
+                        src="/phone.png"
+                        alt="Download to your mobile device"
+                        title="Download to your mobile device"
+                        width="25"
+                        height="25"
+                      />
+                    </a>
+                    <a href="/cart">
+                      <Image
+                        src="/print.png"
+                        alt="Print it out"
+                        title="Print it out"
+                        width="25"
+                        height="25"
+                      />
+                    </a>
+                  </div>
+                  <div className={styles.purchaseDiv}>
+                    <span>Tickets from {comedian.ticketPriceMin}</span>
+                    <a href={`/comedians/${comedian.id}`}>
+                      <span className={styles.buyOneNow}>Buy one now!</span>
+                    </a>
                   </div>
                 </div>
-                <div className={styles.icons}>
-                  <a href="/cart">
-                    <Image
-                      src="/save.png"
-                      alt="Save to your computer"
-                      title="Save to your computer"
-                      width="25"
-                      height="25"
-                    />
-                  </a>
-                  <a href="/cart">
-                    <Image
-                      src="/phone.png"
-                      alt="Download to your mobile device"
-                      title="Download to your mobile device"
-                      width="25"
-                      height="25"
-                    />
-                  </a>
-                  <a href="/cart">
-                    <Image
-                      src="/print.png"
-                      alt="Print it out"
-                      title="Print it out"
-                      width="25"
-                      height="25"
-                    />
-                  </a>
-                </div>
-                <div className={styles.purchaseDiv}>
-                  <span>Tickets from {comedian.ticketPriceMin}</span>
-                  <a href={`/comedians/${comedian.id}`}>
-                    <span className={styles.buyOneNow}>Buy one now!</span>
-                  </a>
+                <div
+                  className={`${styles.comedianDiv} ${styles.comedianDivBack}`}
+                >
+                  {' '}
+                  TEXT
                 </div>
               </div>
             </div>
